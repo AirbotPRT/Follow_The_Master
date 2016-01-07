141bc1a62cc08c4be380e8c5d06cc920ee746860
Jimmy Lucidarme <jlucidar@gmail.com>
2016-01-07 15:51:45 +0100

    started abstractros + minor changes

---
+++ drone_classes.py
@@ -0,0 +1,60 @@
+import abstractros
+
+#Declare the data received from GPS on ROS network
+class Drone():
+	def __init__(id):
+
+		#Not sure => To modify
+		id="/drone"
+
+	#adapter for data from ros
+	def update_data(x,y,z,o):
+		self.x=x
+		self.y=y
+		self.z=z
+		self.o=o
+
+
+
+
+class Drone_slave(Drone):
+	def __init__():
+
+	#Define function to take off
+	def take_off():
+
+
+	#Define function to land
+	def land():
+
+
+
+	#Define function to set which drone is the master
+	def set_master(drone):
+
+
+
+
+	#Define function to set the position of the slave, the same as the master
+	def set_position(x,y,z,Orientation):
+
+
+
+	#Define function to fix the slave altitude when master is moving
+	def fix_altitude():
+
+
+
+	#Define function to fix the slave distance when master is moving
+	def fix_distance():
+
+
+
+	#Define function to fix the slave orientation when master is moving
+	def fix_orientation():
+
+
+
+
+
+
