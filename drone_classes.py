3926a596291e7f33c3d60317dc090d442ac04f47
MichelChevalier <michel229.chevalier@gmail.com>
2016-01-13 19:39:34 +0100

    Drone classes

--- drone_classes.py
+++ drone_classes.py
@@ -1,7 +1,7 @@
 import abstractros
 
 #Declare the data received from GPS on ROS network
-class Drone:
+class Drone(Object):
 	def __init__(self,id):
 
 		
@@ -14,29 +14,32 @@ class Drone:
 		self.z=z
 		self.o=o
 
-
-
-
+		
 class Drone_slave(Drone):
 	def __init__(self):
+		super(Drone, self).__init__()
+		self.master=None
 
 	#Define function to take off
 	def take_off(self):
-
+		abstractros.takeOff
 
 	#Define function to land
 	def land(self):
-
+		abstractros.land
 
 
 	#Define function to set which drone is the master
 	def set_master(self,drone):
-
-
+		self.master=drone
 
 
 	#Define function to set the position of the slave, the same as the master
-	def set_position(self,x,y,z,Orientation):
+	def set_position(self,x,y,z,o):
+		self.x=
+		self.y=
+		self.z=
+		self.o=
 
 
 
