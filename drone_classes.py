de0c55513b2a0936eae92078a0eeaf272b36addd
Jimmy Lucidarme <jlucidar@gmail.com>
2016-01-19 23:35:50 +0100

    complete the fix colinearity and fix distnace algorithms

--- drone_classes.py
+++ drone_classes.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import abstractros
+import math
 
 class Drone(object):
     '''
@@ -19,10 +20,40 @@ class Drone(object):
     @staticmethod
     def refresh_flight_info(drones):
         '''
-            Static function to refresh the data 
+            Static function to refresh the data
         '''
         for drone in drones:
             abstractros.get_info(drone)
+
+    @staticmethod  
+    def calc_distance(drone1, drone2):
+        """
+            function used to calculate the distance between two drones.
+        """
+        distance_x = drone1.x -drone2.x
+        distance_y = drone1.y - drone2.y
+        return math.sqrt(distance_x^2 + distance_y^2)
+
+    @staticmethod 
+    def calc_colinearity(drone1, drone2):
+        """
+            function used to calculate the colinearity error between drone1 and drone2 in deg.
+
+            it represent the angle between drone 1 direction and drone 2.
+        """
+        p1x = drone1.x
+        p1y = drone1.y
+        p2x = drone1.x-Drone.calc_distance(drone1, drone2)*math.sin(drone1.o*2*math.pi/360)
+        p2y = drone1.y-Drone.calc_distance(drone1, drone2)*math.cos(drone1.o*2*math.pi/360)
+        p3x = drone2.x
+        p3y = drone2.y
+
+        p12 = math.sqrt((p1x - p2x)^2 + (p1y - p2y)^2)
+        p13 = math.sqrt((p1x - p3x)^2 + (p1y - p3y)^2)
+        p23 = math.sqrt((p2x - p3x)^2 + (p2y - p3y)^2)
+
+
+        return (math.acos((p12**2 + p13**2 - p23**2) / (2 * p12 * p13)))*(360/(2*math.pi))    
 
 
 
@@ -53,7 +84,7 @@ class DroneSlave(Drone):
         """
             function to go to the (x,y,z,o) point
         """
-        abstractros.goto(x, y, z, o)
+        abstractros.goto(self, x, y, z, o)
 
 
     #Define function to set which drone is the master
@@ -69,7 +100,7 @@ class DroneSlave(Drone):
         """
             function to fix altitude difference between master and self
         """
-        self.goto(self.x, self.y, self.master.z + diff, self.o)
+        self.goto(self, self.x, self.y, self.master.z + diff, self.o)
 
 
     #Define function to fix the slave distance when master is moving
@@ -77,33 +108,42 @@ class DroneSlave(Drone):
         """
             function to fix distance between master and self
         """
-        pass
-        # TODO : ALGO calcul de la position a bonne distance la plus proche
-        # 
-        #
-        #
-        #
+        p1x = self.x
+        p1y = self.y
+        p2x = self.x
+        p2y = self.y+1
+        p3x = self.master.x
+        p3y = self.master.y
 
+        p12 = math.sqrt((p1x - p2x)^2 + (p1y - p2y)^2)
+        p13 = math.sqrt((p1x - p3x)^2 + (p1y - p3y)^2)
+        p23 = math.sqrt((p2x - p3x)^2 + (p2y - p3y)^2)
+        diff_angle = (math.acos((p12**2 + p13**2 - p23**2) / (2 * p12 * p13)))*(360/(2*math.pi))    
 
+        x_target = self.x + (Drone.calc_distance(self.master, self)-dist)*math.cos(diff_angle*2*math.pi/360)
+        y_target = self.y + (Drone.calc_distance(self.master, self)-dist)*math.sin(diff_angle*2*math.pi/360)
+
+        self.goto(self, x_target, y_target, self.z, self.o)
 
     #Define function to fix the slave orientation when master is moving
     def fix_orientation(self, diff):
         """
             function to fix orientation difference between master and self
         """
-        self.goto(self.x, self.y, self.z, self.master.o+diff)
+        self.goto(self, self.x, self.y, self.z, self.master.o+diff)
 
     #Define function to fix the slave direction when master is moving
-    def fix_colinearity(self):
+    def fix_colinearity(self, diff):
         """
             function to fix colinearity between master and self
+
+            the diff arg correspond to the desired angle between the master direction and the slave
         """
-        # TODO : ALGO calcul de la position a atteindre pour corriger l'angle entre la direction du master et le slave
-        # 
-        #
-        #
-        #
-        pass
+        x_target = self.master.x-Drone.calc_distance(self.master, self)*math.sin((self.master.o+diff)*2*math.pi/360)
+        y_target = self.master.y-Drone.calc_distance(self.master, self)*math.cos((self.master.o+diff)*2*math.pi/360)
+
+        # would be better to impose a defined curve, but not possible actually.
+        self.goto(self, x_target, y_target, self.z, self.o)
 
 
 
--- main.py
+++ main.py
@@ -7,8 +7,7 @@ from drone_classes import Drone, DroneSlave
 
 master_d = None
 slave_d = None
-distance= 0
-colinearity=0
+
 
 #initialize the variables
 def init():
@@ -24,33 +23,6 @@ def check_safety_issues():
     """
     pass
 
-
-def calc_distance():
-    """
-        function used to calculate the distance between two drones.
-    """
-    distance_x = master_d.x - slave_d.x
-    distance_y = master_d.y - slave_d.y
-    distance = math.sqrt(distance_x^2 + distance_y^2)
-
-def calc_colinearity():
-    """
-        function used to calculate the colinearity error between two drones in deg.
-    """
-    p1x = master_d.x
-    p1y = master_d.y
-    p2x = master_d.x-distance*math.sin(master_d.o*2*math.pi/360)
-    p2y = master_d.y-distance*math.cos(master_d.o*2*math.pi/360)
-    p3x = slave_d.x
-    p3y = slave_d.y
-
-    p12 = math.sqrt((p1x - p2x)^2 + (p1y - p2y)^2)
-    p13 = math.sqrt((p1x - p3x)^2 + (p1y - p3y)^2)
-    p23 = math.sqrt((p2x - p3x)^2 + (p2y - p3y)^2)
-
-
-    colinearity = (math.acos((p12**2 + p13**2 - p23**2) / (2 * p12 * p13)))*(360/(2*math.pi))
-
 #Slave follow the master
 def slave_control():
     """
@@ -58,23 +30,21 @@ def slave_control():
     """
     Drone.refresh_flight_info([master_d, slave_d])  # Don't like that
     check_safety_issues()
-    calc_distance()
-    calc_colinearity()
 
     # Altitude fix
     if((master_d.z - settings.MARGIN_ALT) > (slave_d.z + settings.ALT_DIFF)) or ((slave_d.z + settings.ALT_DIFF) > (master_d.z + settings.MARGIN_ALT)):
         slave_d.fix_z(settings.ALT_DIFF)
 
     # Distance Fix
-    elif((settings.DIST - settings.MARGIN_DIST) > distance) or (distance > (settings.DIST + settings.MARGIN_DIST)):
+    elif((settings.DIST - settings.MARGIN_DIST) > Drone.calc_distance(master_d, slave_d)) or (Drone.calc_distance(master_d, slave_d) > (settings.DIST + settings.MARGIN_DIST)):
         slave_d.fix_distance(settings.DIST)
 
-    # Orinetation Fix 
+    # Orinetation Fix
     elif((master_d.o - settings.MARGIN_ORIEN) > (slave_d.o + settings.ORIEN_DIFF)) or ((slave_d.o + settings.ORIEN_DIFF) > (master_d.o  + settings.MARGIN_ORIEN)):
-        slave_d.fix_o(settings.ORIEN_DIFF)   
+        slave_d.fix_o(settings.ORIEN_DIFF)
 
     # Coliniearity Fix ( the angle must be 0 if we wants to be colinear)
-    elif((settings.COL_DIFF - settings.MARGIN_COL) > colinearity) or (colinearity > (settings.COL_DIFF  + settings.MARGIN_COL)):
+    elif((settings.COL_DIFF - settings.MARGIN_COL) > Drone.calc_colinearity(master_d, slave_d)) or (Drone.calc_colinearity(master_d, slave_d) > (settings.COL_DIFF  + settings.MARGIN_COL)):
         slave_d.fix_colinearity(settings.COL_DIFF)
 
 
